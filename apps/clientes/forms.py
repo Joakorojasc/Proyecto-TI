fd9c535390e6acad970eb27aa56bef002921fd6f
Diego Da Silva <diego.dasilva@uc.cl>
2026-09-22 19:34:57 -0300

    style: aplicar formato de ruff a forms.py

--- apps/clientes/forms.py
+++ apps/clientes/forms.py
@@ -6,4 +6,4 @@ from .models import Cliente
 class RegistroClienteForm(forms.ModelForm):
     class Meta:
         model = Cliente
-        fields = ['rut', 'razon_social', 'contacto', 'direccion']
+        fields = ["rut", "razon_social", "contacto", "direccion"]
