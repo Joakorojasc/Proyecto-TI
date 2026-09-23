97b695946adc1b4f727c06d5452f9cbf31f10796
Renato <renato.cabello@uc.cl>
2026-09-23 19:35:48 -0300

    Aplica correcciones para pasar linter

--- apps/clientes/urls.py
+++ apps/clientes/urls.py
@@ -3,7 +3,11 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('registro/', views.registro_cliente, name='registro_cliente'),
-    path('login/', auth_views.LoginView.as_view(template_name='clientes/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path("registro/", views.registro_cliente, name="registro_cliente"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="clientes/login.html"),
+        name="login",
+    ),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
