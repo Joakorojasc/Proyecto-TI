a46995ef305e716cd9939446f81cc8f805679069
Renato <renato.cabello@uc.cl>
2026-09-23 18:53:24 -0300

    Se grega pantalla de login(SCRUM-10)

--- apps/clientes/urls.py
+++ apps/clientes/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path("registro/", views.registro_cliente, name="registro_cliente"),
+    path('registro/', views.registro_cliente, name='registro_cliente'),
+    path('login/', auth_views.LoginView.as_view(template_name='clientes/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
