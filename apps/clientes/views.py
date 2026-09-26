30f87e5e452c4139d109c08a3003674c130dced4
Renato <renato.cabello@uc.cl>
2026-09-26 11:29:35 -0300

    Correccion Tes de cobertura

--- apps/clientes/views.py
+++ apps/clientes/views.py
@@ -20,32 +20,35 @@ def registro_cliente(request: HttpRequest) -> HttpResponse:
 
 
 def registro_usuario(request: HttpRequest) -> HttpResponse:
-    user_form: UserCreationForm = UserCreationForm()
-    empresa_form: EmpresaRegistroForm = EmpresaRegistroForm()
-
     if request.method == "POST":
         user_form = UserCreationForm(request.POST)
         empresa_form = EmpresaRegistroForm(request.POST)
 
         if user_form.is_valid() and empresa_form.is_valid():
-            auth_user = user_form.save()
-            grupo_cliente, created = Group.objects.get_or_create(name="Cliente")
-            auth_user.groups.add(grupo_cliente)
-
-            nuevo_cliente = empresa_form.save()
-
-            UsuarioPortal.objects.create(
-                cliente=nuevo_cliente, nombre=auth_user.username, rol="Admin"
-            )
             with transaction.atomic():
                 auth_user = user_form.save()
                 nuevo_cliente = empresa_form.save()
+
+                grupo_cliente, _ = Group.objects.get_or_create(name="Cliente")
+                auth_user.groups.add(grupo_cliente)
+
                 UsuarioPortal.objects.create(
                     usuario=auth_user,
                     cliente=nuevo_cliente,
                     nombre=auth_user.username,
                     rol="Admin",
                 )
-            return redirect("login")
 
-    return render(request, "registro.html", {"user_form": user_form, "empresa_form": empresa_form})
+            return redirect("login")
+    else:
+        user_form = UserCreationForm()
+        empresa_form = EmpresaRegistroForm()
+
+    return render(
+        request,
+        "registro.html",
+        {
+            "user_form": user_form,
+            "empresa_form": empresa_form,
+        },
+    )
